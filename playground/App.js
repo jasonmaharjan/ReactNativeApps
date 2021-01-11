24e076149a3f39edacae71b7942542db94ec6ff6
Jason <jason8maharjan@gmail.com>
2021-01-11 16:40:51 +0545

    Working with Images and colors

--- playground/App.js
+++ playground/App.js
@@ -1,76 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Image, ImageBackground } from 'react-native';
+
+import { styles } from './app.styles';
 
 export default function App() {
   return (
-    <View style = {styles.container}>
+    <View style = {styles.view}>
+      <ImageBackground source = {require('./assets/1.jpg')} style = {styles.titleContainer} >
+        <Text style = {styles.title}>Flex Test</Text>
+      </ImageBackground>
+      <View style = {styles.container}> 
 
-      <Text style = {styles.title}>Flex Test</Text>
+        <View style = {styles.boxContainer}>
+          <View style={styles.container1}>
+            <Text style = {styles.text1}>View 1</Text>
+            <StatusBar style="auto" /> 
+          </View>
 
-      <View style={styles.container1}>
-        <Text style = {styles.text1}>View 1</Text>
-        <StatusBar style="auto" /> 
-      </View>
+          <View style={styles.container2}>
+            <Text style = {styles.text2}>View 2</Text>
+            <StatusBar style="auto" /> 
+          </View>
 
-      <View style={styles.container2}>
-        <Text style = {styles.text2}>View 2</Text>
-        <StatusBar style="auto" /> 
-      </View>
+          <View style={styles.container3}>
+            <Text style = {styles.text3}>View 3</Text>
+            <StatusBar style="auto" /> 
+          </View>
+        </View>
 
-      <View style={styles.container3}>
-        <Text style = {styles.text3}>View 3</Text>
-        <StatusBar style="auto" /> 
-      </View>
+        <View style = {styles.imageContainer}>
+          <Image source={require('./assets/1.jpg')} style = {styles.image} />
+          <Image source={require('./assets/3.jpg')} style = {styles.image} />
+          <Image source={require('./assets/4.jpg')} style = {styles.image} />
+          <Image source={require('./assets/5.jpg')} style = {styles.image} />
+        </View>
 
+      </View>        
     </View>
     
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 120,
-    marginHorizontal: 30,
-    marginBottom: 20,
-  },
-
-  title: {
-    height: 40,
-    textAlign: 'center'
-  },  
-
-  container1: {
-    flex: 1,
-    backgroundColor: "red",
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  text1: {
-    color: 'white',
-  },
-
-  container2: {
-    backgroundColor: "orange",
-    flex: 2,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-    
-  text2: {
-    color: 'white',
-  },
-
-  container3: {
-    flex: 3,
-    backgroundColor: "green",
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  text3: {
-    color: 'white',
-  }
-});
